70b903a8d72a953c37c880df49f902cb9b13c621
Geir Arne Hjelle <geirarne@gmail.com>
2025-06-04 16:01:42 +0200

    Fix dynamic parsing uses local names without full path (#7)

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@
 import json
 from pathlib import Path
 from types import ModuleType
+from typing import Type
 
 import pydantic
 import pytest
@@ -11,8 +12,55 @@ import tomli_w
 from configaroo import Configuration
 
 
+# Configuration schema
+class StrictSchema(pydantic.BaseModel):
+    model_config = pydantic.ConfigDict(extra="forbid")
+
+
+class DeeplyNestedSchema(StrictSchema):
+    sea: str
+
+
+class NestedSchema(StrictSchema):
+    pie: float
+    seven: int
+    deep: DeeplyNestedSchema
+
+
+class LogSchema(StrictSchema):
+    level: str
+    format: str
+
+
+class WithDotSchema(StrictSchema):
+    org_num: int = pydantic.Field(alias="org.num")
+
+
+class PathsSchema(StrictSchema):
+    relative: Path
+    dynamic: Path
+    absolute: Path
+
+
+class ConfigSchema(StrictSchema):
+    number: int
+    word: str
+    phrase: str
+    things: list[str]
+    nested: NestedSchema
+    log: LogSchema
+    with_dot: WithDotSchema
+    paths: PathsSchema
+
+
 @pytest.fixture
-def config():
+def model() -> Type[ConfigSchema]:
+    """A schema for the test configuration"""
+    return ConfigSchema
+
+
+@pytest.fixture
+def config() -> Configuration:
     """A test configuration"""
     return Configuration(
         number=42,
@@ -20,43 +68,14 @@ def config():
         phrase="The meaning of life is {number}",
         things=["house", "car", "kayak"],
         nested={"pie": 3.14, "seven": 7, "deep": {"sea": "Marianer"}},
+        log={"level": "info", "format": "<level>{level:<8} {message}</level>"},
         with_dot={"org.num": 1234},
         paths=Configuration(
             relative="src/configaroo/configuration.py",
-            dynamic="{project_path}/tests/test_configuration.py",
+            dynamic="{project_path}/tests/test_dynamic.py",
             absolute="/home/configaroo",
         ),
     )
-
-
-@pytest.fixture
-def model():
-    """A test model"""
-
-    class NestedModel(pydantic.BaseModel):
-        pie: float
-        seven: int
-
-    # Use create_model() to include a field with a dot in the name
-    WithDotModel = pydantic.create_model("WithDotModel", **{"org.num": int})
-
-    class PathsModel(pydantic.BaseModel):
-        relative: Path
-        dynamic: Path
-        absolute: Path
-
-    class ConfigModel(pydantic.BaseModel):
-        model_config = pydantic.ConfigDict(extra="forbid")
-
-        number: int
-        word: str
-        phrase: str
-        things: list[str]
-        nested: NestedModel
-        with_dot: WithDotModel
-        paths: PathsModel
-
-    return ConfigModel
 
 
 @pytest.fixture
@@ -89,7 +108,7 @@ def other_json_path(base_path, config) -> Path:
     return write_file(base_path / "files" / "jsonfile", json, config)
 
 
-def write_file(path: Path, lib: ModuleType, config: Configuration):
+def write_file(path: Path, lib: ModuleType, config: Configuration) -> Path:
     """Write a configuration to file. Return path for convenience"""
     path.write_text(lib.dumps(config.to_dict()), encoding="utf-8")
     return path
